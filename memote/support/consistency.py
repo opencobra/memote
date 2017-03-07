ffa546d05b4662afaedfc3e4158b13b8f6f4d8df
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-03-07 19:28:31 +0100

    add first draft of inconsistent minimal sets

--- memote/support/consistency.py
+++ memote/support/consistency.py
@@ -26,10 +26,12 @@ from operator import attrgetter
 
 import numpy as np
 import sympy
-from nump.linalg import svd
+from numpy.linalg import svd
 from six import iteritems
 
-__all__ = ("check_stoichiometric_consistency", "find_unconserved_metabolites")
+__all__ = (
+    "check_stoichiometric_consistency", "find_unconserved_metabolites",
+    "find_inconsistent_min_stoichiometry")
 
 LOGGER = logging.getLogger(__name__)
 
@@ -164,15 +166,15 @@ def find_unconserved_metabolites(model):
     LOGGER.debug("model has %d internal reactions", len(internal_rxns))
     # The binary variables k[i] in the paper.
     k_vars = list()
-    for metabolite in metabolites:
+    for met in metabolites:
         # The element m[i] of the mass vector.
-        m_var = Variable(metabolite.id)
-        k_var = Variable("k_{}".format(metabolite.id), type="binary")
+        m_var = Variable(met.id)
+        k_var = Variable("k_{}".format(met.id), type="binary")
         k_vars.append(k_var)
         stoich_trans.add([m_var, k_var])
         # This constraint is equivalent to 0 <= k[i] <= m[i].
         stoich_trans.add(Constraint(
-            k_var - m_var, ub=0, name="switch_{}".format(metabolite.id)))
+            k_var - m_var, ub=0, name="switch_{}".format(met.id)))
     stoich_trans.update()
     add_reaction_constraints(stoich_trans, internal_rxns, Constraint)
     # The objective is to maximize the binary indicators k[i], subject to the
@@ -193,18 +195,7 @@ def find_unconserved_metabolites(model):
             " (only optimal or infeasible expected).".format(status))
 
 
-def unconserved_minimal_set(metabolite_index, reaction_index, nullspace):
-    """
-    Find minimal unconserved sets.
-    """
-    # create continuous y vars
-    # create binary k vars
-    # constrain y - k to lower bound 0
-    # add nullspace constraints
-    # constrain y > 0 (y >= epsilon)
-    # minimize
-
-def find_inconsistent_min_stoichiometry(model, tol=1e-15):
+def find_inconsistent_min_stoichiometry(model, tol=1e-13):
     """
     Find minimal unbalanced reaction sets.
 
@@ -217,8 +208,51 @@ def find_inconsistent_min_stoichiometry(model, tol=1e-15):
            "Detection of Stoichiometric Inconsistencies in Biomolecular Models."
            Bioinformatics 24, no. 19 (2008): 2245.
     """
+    def create_milp_problem():
+        ns_problem = Model()
+        k_vars = list()
+        for met in metabolites:
+            # The element y[i] of the mass vector.
+            y_var = Variable(met.id)
+            k_var = Variable("k_{}".format(met.id), type="binary")
+            k_vars.append(k_var)
+            ns_problem.add([y_var, k_var])
+            # This constraint is equivalent to 0 <= y[i] <= k[i].
+            ns_problem.add(Constraint(
+                y_var - k_var, lb=0, name="switch_{}".format(met.id)))
+        ns_problem.update()
+        # add nullspace constraints
+        for (i, row) in enumerate(left_ns):
+            if (row == 0.0).all():
+                # singleton set!
+                continue
+            met = met_inv_map[i]
+            y_var = ns_problem.variables[met_inv_map[i].id]
+            expression = sympy.Add(*[coef * y_var for coef in row if coef != 0.0])
+            constraint = Constraint(expression, lb=0, ub=0,
+                                    name="ns_{}".format(met.id))
+            ns_problem.add(constraint)
+        # The objective is to minimize the binary indicators k[i], subject to the
+        # above inequality constraints.
+        ns_problem.objective = Objective(1)
+        ns_problem.objective.set_linear_coefficients(
+            {k_var: 1. for k_var in k_vars})
+        ns_problem.objective.direction = "min"
+        return ns_problem, k_vars
+
+    def add_cut(non_zero):
+        # non_zero corresponds to 'P' in the paper
+        expr = sympy.Add(*k_vars)
+        constr = Constraint(expr, ub=non_zero - 1)
+        problem.add(constr)
+
+
     if check_stoichiometric_consistency(model):
         return set()
+    Model = model.solver.interface.Model
+    Constraint = model.solver.interface.Constraint
+    Variable = model.solver.interface.Variable
+    Objective = model.solver.interface.Objective
     unconserved_mets = find_unconserved_metabolites(model)
     internal_rxns = set(model.reactions) - set(model.exchanges)
     internal_mets = set(met for rxn in internal_rxns for met in rxn.metabolites)
@@ -228,13 +262,27 @@ def find_inconsistent_min_stoichiometry(model, tol=1e-15):
     reactions = sorted(internal_rxns, key=get_id)
     metabolites = sorted(internal_mets, key=get_id)
     stoich, met_index, rxn_index = stoichiometry_matrix(metabolites, reactions)
+    met_inv_map = {i: met for (met, i) in iteritems(met_index)}
     left_ns = nullspace(stoich.T)
+    # deal with numerical instabilities
+    left_ns[np.abs(left_ns) < tol] = 0.0
     inc_minimal = set()
     for met in unconserved_mets:
         row = met_index[met]
-        if (left_ns[row] < tol).all():
-            # singleton set
+        if (left_ns[row] == 0.0).all():
+            LOGGER.debug("%s: singleton minimal unconservable set", met.id)
+            # singleton set!
             inc_minimal.add((met,))
             continue
-
-    return set()
+        problem, k_vars = create_milp_problem()
+        status = problem.optimize()
+        while status == "optimal":
+            LOGGER.debug("%s: status %s", met.id, status)
+            solution = [model.metabolites.get_by_id(var.name[2:])
+                        for var in k_vars if var.primal > 0.1]
+            LOGGER.debug("%s: set size %d", met.id, len(solution))
+            inc_minimal.add(tuple(solution))
+            add_cut(len(solution))
+            status = problem.optimize()
+        LOGGER.debug("%s: last status %s", met.id, status)
+    return inc_minimal
