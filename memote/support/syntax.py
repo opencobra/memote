6f43ac748640a170eb5c0e1cb34b65be25c5a2e4
Christian Lieven <christian.lieven@rwth-aachen.de>
2017-03-10 14:35:10 +0100

    Implement syntax protocol [fix #5] (#20)
    * add tests and checks for ABC-transporter tags and upper case metabolite IDs
    
    * Add checks and tests for exchange and demand reactions and for tagged reactions that shouldn't be tagged

--- memote/support/syntax.py
+++ memote/support/syntax.py
@@ -82,6 +82,45 @@ def find_rxn_id_compartment_suffix(model, suffix):
     return [rxn for rxn in rxns if not comp_pattern.match(rxn.id)]
 
 
+def find_rxn_id_suffix_compartment(model, suffix):
+    """
+    Find non-transport reactions whose ID bear a compartment tag but which do
+    not contain any metabolites in the given compartment.
+
+    Parameters
+    ----------
+    model : cobra.core.Model.Model
+        A cobrapy metabolic model.
+    suffix : str
+        The suffix of the compartment to be checked.
+
+    Returns
+    -------
+    list
+        Non-transport reactions that have at least one metabolite in the
+        compartment given by `suffix` but whose IDs do not have
+        the `suffix` appended.
+    """
+
+    transport_rxns = find_transport_reactions(model)
+    exchange_demand_rxns = find_demand_and_exchange_reactions(model)
+
+    comp_pattern = re.compile(
+        "[A-Z0-9]+\w*?{}\w*?".format(SUFFIX_MAP[suffix])
+    )
+
+    rxns = []
+
+    for rxn in model.reactions:
+        if comp_pattern.match(rxn.id):
+            if ('biomass' not in rxn.id.lower()) and (
+                    rxn not in transport_rxns and
+                    rxn not in exchange_demand_rxns):
+                rxns.append(rxn)
+
+    return [rxn for rxn in rxns if suffix not in rxn.compartments]
+
+
 def find_reaction_tag_transporter(model):
     """Return a list of transport reactions that have not been tagged as such.
 
@@ -107,3 +146,127 @@ def find_reaction_tag_transporter(model):
 
     return [rxn for rxn in non_abc_transporters
             if not re.match("[A-Z0-9]+\w*?t\w*?", rxn.id)]
+
+
+def find_abc_tag_transporter(model):
+    """Return a list of Atp-binding cassette transport rxns that have not been
+    tagged with 'abc'.
+
+
+    An ABC transport reaction is defined as follows:
+       -- It contains metabolites from at least 2 compartments
+       -- At least 1 metabolite undergoes no chemical reaction
+          i.e. the formula stays the same on both sides of the equation
+       -- ATP is converted to ADP (+ Pi + H,
+          yet this isn't checked for explicitly)
+
+    Reactions that only transport protons ('H') across the membrane are
+    excluded, as well as reactions with redox cofactors whose formula is
+    either 'X' or 'XH2'
+
+    Parameters
+    ----------
+    model : model: cobra.core.Model.Model
+            A cobrapy metabolic model
+    """
+    transport_rxns = find_transport_reactions(model)
+    atp_adp_rxns = find_atp_adp_converting_reactions(model)
+
+    abc_transporters = set(transport_rxns).intersection(set(atp_adp_rxns))
+
+    return [rxn for rxn in abc_transporters
+            if not re.match("[A-Z0-9]+\w*?abc\w*?", rxn.id)]
+
+
+def find_upper_case_mets(model):
+    """Return a list of metabolites whose ID roots contain uppercase
+    characters
+
+    In metabolite names, individual uppercase exceptions are allowed:
+    -- Dextorotary prefix: 'D'
+    -- Levorotary prefix: 'L'
+    -- Prefixes for the absolute configuration of a stereocenter: 'R' and 'S'
+    -- Prefixes for the absolute stereochemistry of double bonds 'E' and 'Z'
+    -- Acyl carrier proteins can be labeled as 'ACP'
+
+    Parameters
+    ----------
+    model : model: cobra.core.Model.Model
+            A cobrapy metabolic model
+    """
+    comp_pattern = "^([a-z0-9]|Z|E|L|D|ACP|S|R)+_\w+"
+    return [met for met in model.metabolites
+            if not re.match(comp_pattern, met.id)]
+
+
+def find_untagged_demand_rxns(model):
+    """Return a list of demand reactions whose IDs do not begin with 'DM_',
+    the customary tag.
+
+    [1] defines demand reactions as:
+    -- 'unbalanced network reactions that allow the accumulation of a compound'
+    -- reactions that are chiefly added during the gap-filling process
+    -- as a means of dealing with 'compounds that are known to be produced by
+    the organism [..] (i) for which no information is available about their
+    fractional distribution to the biomass or (ii) which may only be produced
+    in some environmental conditions
+    -- reactions with a formula such as: 'met_c -> '
+
+    Demand reactions differ from exchange reactions in that the metabolites
+    are not removed from the extracellular environment, but from any of the
+    organism's compartments.
+
+    Parameters
+    ----------
+    model : model: cobra.core.Model.Model
+            A cobrapy metabolic model
+
+    References
+    ----------
+    [1] Thiele, I., & Palsson, B. Ø. (2010, January). A protocol for generating
+    a high-quality genome-scale metabolic reconstruction. Nature protocols.
+    Nature Publishing Group. http://doi.org/10.1038/nprot.2009.203
+    """
+    demand_and_exchange_rxns = find_demand_and_exchange_reactions(model)
+    demand_rxns = [rxn for rxn in demand_and_exchange_rxns
+                   if not rxn.reversibility and
+                   rxn.get_compartments() not in ['e']]
+
+    comp_pattern = "^DM_\w*?"
+    return [rxn for rxn in demand_rxns
+            if not re.match(comp_pattern, rxn.id)]
+
+
+def find_untagged_exchange_rxns(model):
+    """Return a list of exchange reactions whose IDs do not begin with 'EX_',
+    the customary tag.
+
+    [1] defines exchange reactions as:
+    -- reactions that 'define the extracellular environment'
+    -- 'unbalanced, extra-organism reactions that represent the supply to or
+    removal of metabolites from the extra-organism "space"'
+    -- reactions with a formula such as: 'met_e -> ' or ' -> met_e' or
+    'met_e <=> '
+
+    Exchange reactions differ from demand reactions in that the metabolites
+    are removed from or added to the extracellular environment only. With this
+    the updake or secretion of a metabolite is modeled, respectively.
+
+    Parameters
+    ----------
+    model : model: cobra.core.Model.Model
+            A cobrapy metabolic model
+
+    References
+    ----------
+    [1] Thiele, I., & Palsson, B. Ø. (2010, January). A protocol for generating
+    a high-quality genome-scale metabolic reconstruction. Nature protocols.
+    Nature Publishing Group. http://doi.org/10.1038/nprot.2009.203
+    """
+    demand_and_exchange_rxns = find_demand_and_exchange_reactions(model)
+    exchange_rxns = [rxn for rxn in demand_and_exchange_rxns
+                     if rxn.get_compartments() == ['e']]
+
+    comp_pattern = "^EX_\w*?"
+    return [rxn for rxn in exchange_rxns
+            if not re.match(comp_pattern, rxn.id)]
--- tests/test_for_support/test_for_syntax.py
+++ tests/test_for_support/test_for_syntax.py
@@ -29,6 +29,7 @@ Tests ensuring that the functions in `memote.support.syntax` work as expected.
 
 
 def model_builder(name):
+    """Returns a cobra.model built to reflect the required test case"""
     model = cobra.Model(id_or_model=name, name=name)
     if name == "rxn_correct_tags":
         for i, pairs in enumerate(syntax.SUFFIX_MAP.items()):
@@ -37,9 +38,9 @@ def model_builder(name):
             else:
                 rxn = cobra.Reaction('R{}{}'.format(i, pairs[1]))
             rxn.add_metabolites(
-                {cobra.Metabolite(id="M{0:d}_{1:s}".format(i, pairs[0]),
+                {cobra.Metabolite(id="m{0:d}_{1:s}".format(i, pairs[0]),
                                   compartment=pairs[0]): -1,
-                 cobra.Metabolite(id="B{0:d}_{1:s}".format(i, pairs[0]),
+                 cobra.Metabolite(id="b{0:d}_{1:s}".format(i, pairs[0]),
                                   compartment=pairs[0]): 1}
             )
             model.add_reaction(rxn)
@@ -49,9 +50,32 @@ def model_builder(name):
         for i, pairs in enumerate(syntax.SUFFIX_MAP.items()):
             rxn = cobra.Reaction('R{}'.format(i))
             rxn.add_metabolites(
-                {cobra.Metabolite(id="M{0:d}_{1:s}".format(i, pairs[0]),
+                {cobra.Metabolite(id="m{0:d}_{1:s}".format(i, pairs[0]),
                                   compartment=pairs[0]): -1,
-                 cobra.Metabolite(id="B{0:d}_{1:s}".format(i, pairs[0]),
+                 cobra.Metabolite(id="b{0:d}_{1:s}".format(i, pairs[0]),
+                                  compartment=pairs[0]): 1}
+            )
+            model.add_reaction(rxn)
+        return model
+
+    if name == "rxn_tags_but_wrong_compartments":
+        misaligned_suffix_map = dict({'p': 'c',
+                                      'c': 'e',
+                                      'e': 'er',
+                                      'er': 'g',
+                                      'g': 'l',
+                                      'l': 'm',
+                                      'm': 'n',
+                                      'n': 'x',
+                                      'x': 'v',
+                                      'v': 'pp'})
+
+        for i, pairs in enumerate(misaligned_suffix_map.items()):
+            rxn = cobra.Reaction('R{}{}'.format(i, pairs[1]))
+            rxn.add_metabolites(
+                {cobra.Metabolite(id="m{0:d}_{1:s}".format(i, pairs[0]),
+                                  compartment=pairs[0]): -1,
+                 cobra.Metabolite(id="b{0:d}_{1:s}".format(i, pairs[0]),
                                   compartment=pairs[0]): 1}
             )
             model.add_reaction(rxn)
@@ -61,37 +85,37 @@ def model_builder(name):
         # Simple diffusion
         rxn = cobra.Reaction('Rt')
         rxn.add_metabolites(
-            {cobra.Metabolite(id="M_m", formula='C6H12O6',
+            {cobra.Metabolite(id="m_m", formula='C6H12O6',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C6H12O6',
+             cobra.Metabolite(id="m_c", formula='C6H12O6',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn)
         # Anti-Port
         rxn2 = cobra.Reaction('R2t')
         rxn2.add_metabolites(
-            {cobra.Metabolite(id="D_c", formula='H',
+            {cobra.Metabolite(id="d_c", formula='H',
                               compartment='c'): -1,
-             cobra.Metabolite(id="D_m", formula='H',
+             cobra.Metabolite(id="d_m", formula='H',
                               compartment='m'): 1,
-             cobra.Metabolite(id="M_m", formula='C6H12O6',
+             cobra.Metabolite(id="m_m", formula='C6H12O6',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C6H12O6',
+             cobra.Metabolite(id="m_c", formula='C6H12O6',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn2)
         # One-Metabolite transported
         rxn3 = cobra.Reaction('R3t')
         rxn3.add_metabolites(
-            {cobra.Metabolite(id="D_c", formula='C21H26N7O14P2',
+            {cobra.Metabolite(id="d_c", formula='C21H26N7O14P2',
                               compartment='c'): -1,
-             cobra.Metabolite(id="A_c", formula='C21H27N7O14P2',
+             cobra.Metabolite(id="a_c", formula='C21H27N7O14P2',
                               compartment='c'): 1,
-             cobra.Metabolite(id="C_c", formula='H',
+             cobra.Metabolite(id="c_c", formula='H',
                               compartment='c'): 1,
-             cobra.Metabolite(id="M_m", formula='C6H12O6',
+             cobra.Metabolite(id="m_m", formula='C6H12O6',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C6H12O6',
+             cobra.Metabolite(id="m_c", formula='C6H12O6',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn3)
@@ -101,37 +125,37 @@ def model_builder(name):
         # Simple diffusion
         rxn = cobra.Reaction('R')
         rxn.add_metabolites(
-            {cobra.Metabolite(id="M_m", formula='C6H12O6',
+            {cobra.Metabolite(id="m_m", formula='C6H12O6',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C6H12O6',
+             cobra.Metabolite(id="m_c", formula='C6H12O6',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn)
         # Anti-Port
         rxn2 = cobra.Reaction('R2')
         rxn2.add_metabolites(
-            {cobra.Metabolite(id="D_c", formula='H',
+            {cobra.Metabolite(id="d_c", formula='H',
                               compartment='c'): -1,
-             cobra.Metabolite(id="D_m", formula='H',
+             cobra.Metabolite(id="d_m", formula='H',
                               compartment='m'): 1,
-             cobra.Metabolite(id="M_m", formula='C6H12O6',
+             cobra.Metabolite(id="m_m", formula='C6H12O6',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C6H12O6',
+             cobra.Metabolite(id="m_c", formula='C6H12O6',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn2)
         # One-Metabolite transported
         rxn3 = cobra.Reaction('R3')
         rxn3.add_metabolites(
-            {cobra.Metabolite(id="D_c", formula='C21H26N7O14P2',
+            {cobra.Metabolite(id="d_c", formula='C21H26N7O14P2',
                               compartment='c'): -1,
-             cobra.Metabolite(id="A_c", formula='C21H27N7O14P2',
+             cobra.Metabolite(id="a_c", formula='C21H27N7O14P2',
                               compartment='c'): 1,
-             cobra.Metabolite(id="C_c", formula='H',
+             cobra.Metabolite(id="c_c", formula='H',
                               compartment='c'): 1,
-             cobra.Metabolite(id="M_m", formula='C6H12O6',
+             cobra.Metabolite(id="m_m", formula='C6H12O6',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C6H12O6',
+             cobra.Metabolite(id="m_c", formula='C6H12O6',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn3)
@@ -149,9 +173,29 @@ def model_builder(name):
                               compartment='c'): 1,
              cobra.Metabolite(id="h_c", formula='H',
                               compartment='c'): 1,
-             cobra.Metabolite(id="M_m", formula='C7H13NO2',
+             cobra.Metabolite(id="m_m", formula='C7H13NO2',
                               compartment='m'): -1,
-             cobra.Metabolite(id="M_c", formula='C7H13NO2',
+             cobra.Metabolite(id="m_c", formula='C7H13NO2',
+                              compartment='c'): 1}
+        )
+        model.add_reaction(rxn)
+        return model
+
+    if name == "trxn_no_tag_atp_driven":
+        # ATP-driven
+        rxn = cobra.Reaction('R')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="atp_c", formula='C10H12N5O13P3',
+                              compartment='c'): -1,
+             cobra.Metabolite(id="adp_c", formula='C10H12N5O10P2',
+                              compartment='c'): 1,
+             cobra.Metabolite(id="p_c", formula='HO4P',
+                              compartment='c'): 1,
+             cobra.Metabolite(id="h_c", formula='H',
+                              compartment='c'): 1,
+             cobra.Metabolite(id="m_m", formula='C7H13NO2',
+                              compartment='m'): -1,
+             cobra.Metabolite(id="m_c", formula='C7H13NO2',
                               compartment='c'): 1}
         )
         model.add_reaction(rxn)
@@ -173,16 +217,105 @@ def model_builder(name):
         model.add_reaction(rxn)
         return model
 
+    if name == "lower_case_mets":
+        rxn = cobra.Reaction('RLOM')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="abcdeACP_c"): -1,
+             cobra.Metabolite(id="fghij__L_c"): -1,
+             cobra.Metabolite(id="klmnop_R_p"): 2,
+             cobra.Metabolite(id="qrSt_uvw_E_c"): 1,
+             cobra.Metabolite(id="xyZ123456789_c"): 1}
+        )
+        model.add_reaction(rxn)
+        return model
+
+    if name == "upper_case_mets":
+        rxn = cobra.Reaction('RHIM')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="AbcdeACP_c"): -1,
+             cobra.Metabolite(id="fGHij__L_c"): -1,
+             cobra.Metabolite(id="KLMNOP_R_p"): 2,
+             cobra.Metabolite(id="QRSt_uvw_E_c"): 1,
+             cobra.Metabolite(id="xYZ123456789_c"): 1}
+        )
+        model.add_reaction(rxn)
+        return model
+
+    if name == "correct_demand_tag":
+        rxn = cobra.Reaction('DM_abc_c')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="abc_c",
+                              compartment='c'): -1}
+        )
+        model.add_reaction(rxn)
+        return model
+
+    if name == "incorrect_demand_tag":
+        rxn = cobra.Reaction('EX_abc_c')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="abc_c",
+                              compartment='c'): -1}
+        )
+        rxn1 = cobra.Reaction('def_c')
+        rxn1.add_metabolites(
+            {cobra.Metabolite(id="def_c",
+                              compartment='c'): -1}
+        )
+        model.add_reactions([rxn, rxn1])
+        return model
+
+    if name == "correct_exchange_tag":
+        rxn = cobra.Reaction('EX_abc_e')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="abc_e",
+                              compartment='e'): -1}
+        )
+        model.add_reaction(rxn)
+        return model
+
+    if name == "incorrect_exchange_tag":
+        rxn = cobra.Reaction('DM_ghi_e')
+        rxn.add_metabolites(
+            {cobra.Metabolite(id="ghi_e",
+                              compartment='e'): -1}
+        )
+        rxn1 = cobra.Reaction('jkm_e')
+        rxn1.add_metabolites(
+            {cobra.Metabolite(id="jkm_e",
+                              compartment='e'): -1}
+        )
+        model.add_reactions([rxn, rxn1])
+        return model
+
 
 @pytest.mark.parametrize("model, num", [
     ("rxn_correct_tags", 0),
     ("rxn_no_tags", 1)
 ], indirect=["model"])
 def test_non_transp_rxn_id_compartment_suffix_match(model, num):
+    """Expect all rxns outside of the cytosol to be tagged accordingly"""
     for compartment in model.compartments:
         if compartment != 'c':
             rxn_lst = syntax.find_rxn_id_compartment_suffix(model, compartment)
             assert len(rxn_lst) == num
+
+
+@pytest.mark.parametrize("model, num", [
+    ("rxn_correct_tags", 0),
+    ("rxn_tags_but_wrong_compartments", 1)
+], indirect=["model"])
+def test_non_transp_rxn_id_suffix_compartment_match(model, num):
+    """
+    Expect all rxns that are tagged to be in a compartment to at least
+    partially involve mets from that compartment
+    """
+    for compartment in model.compartments:
+        if compartment != 'c':
+            mislab_rxns = syntax.find_rxn_id_suffix_compartment(
+                model, compartment
+            )
+            assert \
+                len(mislab_rxns) == num
 
 
 @pytest.mark.parametrize("model, num", [
@@ -192,5 +325,46 @@ def test_non_transp_rxn_id_compartment_suffix_match(model, num):
     ("proton_pump", 0)
 ], indirect=["model"])
 def test_non_abc_transp_rxn_tag_match(model, num):
+    """Expect all non-abc transport rxns to be tagged with a 't'"""
     trxn_lst = syntax.find_reaction_tag_transporter(model)
     assert len(trxn_lst) == num
+
+
+@pytest.mark.parametrize("model, num", [
+    ("trxn_correct_atp_driven", 0),
+    ("trxn_no_tag_atp_driven", 1)
+], indirect=["model"])
+def test_abc_transp_rxn_tag_match(model, num):
+    """Expect all abc transport rxns to be tagged with 'abc'"""
+    untagged_atp_transport_rxns = syntax.find_abc_tag_transporter(model)
+    assert len(untagged_atp_transport_rxns) == num
+
+
+@pytest.mark.parametrize("model, num", [
+    ("lower_case_mets", 0),
+    ("upper_case_mets", 5)
+], indirect=["model"])
+def test_upper_case_mets(model, num):
+    """Expect all metabolites to be lower case within accepted exceptions"""
+    upper_case_mets = syntax.find_upper_case_mets(model)
+    assert len(upper_case_mets) == num
+
+
+@pytest.mark.parametrize("model, num", [
+    ("correct_demand_tag", 0),
+    ("incorrect_demand_tag", 2)
+], indirect=["model"])
+def test_demand_reaction_tag_match(model, num):
+    """Expect all demand rxn IDs to be prefixed with 'DM_'"""
+    falsely_tagged_demand_rxns = syntax.find_untagged_demand_rxns(model)
+    assert len(falsely_tagged_demand_rxns) == num
+
+
+@pytest.mark.parametrize("model, num", [
+    ("correct_exchange_tag", 0),
+    ("incorrect_exchange_tag", 2)
+], indirect=["model"])
+def test_exchange_reaction_tag_match(model, num):
+    """Expect all exchange rxn IDs to be prefixed with 'EX_'"""
+    falsely_tagged_exchange_rxns = syntax.find_untagged_exchange_rxns(model)
+    assert len(falsely_tagged_exchange_rxns) == num
