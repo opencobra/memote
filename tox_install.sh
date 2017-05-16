535ef65a8a60c17e50ef4d8430cae77a26982da3
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-05-16 18:08:47 +0200

    fix: avoid installing libsbml for flake8 checks

--- tox_install.sh
+++ tox_install.sh
@@ -2,6 +2,6 @@
 
 pip "$@"
 # On Travis CI quickly install python-libsbml wheels.
-if [[ -n "${CI}" ]]; then
+if [[ -n "${CI}" && "${TOXENV}" != "flake8" ]]; then
     pip install --find-links https://s3.eu-central-1.amazonaws.com/moonlight-science/wheelhouse/index.html --no-index python-libsbml==5.12.1
 fi
