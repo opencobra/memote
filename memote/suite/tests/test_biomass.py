6eb036f339aa159a94e762c0b6178312be5dd8a4
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2017-05-29 12:35:56 +0200

    feat: parametrize biomass tests

--- memote/suite/tests/test_biomass.py
+++ memote/suite/tests/test_biomass.py
@@ -15,11 +15,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Supporting functions for stoichiometric consistency checks."""
+"""
+Supporting functions for stoichiometric consistency checks.
+
+N.B.: We parametrize each function here with the identified biomass reactions.
+In the storage of test results we rely on the order of the biomass fixtures
+to remain the same as the parametrized test cases.
+"""
 
 from __future__ import absolute_import
-
-from builtins import zip
 
 import pytest
 import numpy as np
@@ -31,67 +35,75 @@ from memote.support.helpers import find_biomass_reaction
 
 @pytest.fixture(scope="module")
 def biomass_reactions(read_only_model):
-    """Provide any biomass reactions found in the model."""
+    """Return any biomass reactions found in the model."""
     return find_biomass_reaction(read_only_model)
 
 
-def test_biomass_presence(biomass_reactions, store):
+@pytest.fixture(scope="module")
+def biomass_ids(biomass_reactions):
+    """Return any biomass reaction IDs found in the model."""
+    return [rxn.id for rxn in biomass_reactions]
+
+
+def test_biomass_presence(biomass_ids, store):
     """Expect the model to contain at least one biomass reaction."""
-    store["biomass_reactions"] = [rxn.id for rxn in biomass_reactions]
-    assert len(biomass_reactions) > 0, \
+    store["biomass_ids"] = biomass_ids
+    assert len(biomass_ids) > 0, \
         "Could not identify any biomass reaction." \
         " Please change the intended reaction ID(s) to contain 'biomass'."
 
 
-def test_biomass_consistency(biomass_reactions, store):
+@pytest.mark.parametrize("reaction", biomass_reactions, ids=biomass_ids)
+def test_biomass_consistency(reaction, store):
     """Expect biomass components to sum up to 1 g / mmol / h."""
-    store["biomass_sum"] = [
-        biomass.sum_biomass_weight(rxn) for rxn in biomass_reactions]
-    for rxn, cntrl_sum in zip(store["biomass_reactions"], store["biomass_sum"]):
-        assert np.isclose(cntrl_sum, 1.0, atol=1e-03), \
-            "{}'s components sum up to {} which is too far from 1 g / mmol / h"\
-            "".format(rxn, cntrl_sum)
+    store["biomass_sum"] = store.get("biomass_sum", list())
+    component_sum = biomass.sum_biomass_weight(reaction)
+    store["biomass_sum"].append(component_sum)
+    assert np.isclose(component_sum, 1.0, atol=1e-03), \
+        "{}'s components sum up to {} which is too far from 1 mmol / g / h"\
+        "".format(reaction.id, component_sum)
 
 
-def test_biomass_default_production(model, biomass_reactions, store):
+@pytest.mark.parametrize("reaction", biomass_reactions, ids=biomass_ids)
+def test_biomass_default_production(model, reaction, store):
     """Expect biomass production in default medium."""
-    store["biomass_default_flux"] = list()
-    for rxn in biomass_reactions:
-        model.objective = rxn
-        try:
-            solution = model.optimize()
-            store["biomass_default_flux"].append(solution.fluxes[rxn.id])
-        except Infeasible:
-            store["biomass_default_flux"].append(np.nan)
-    for flux in store["biomass_default_flux"]:
-        assert flux > 0.0
+    store["biomass_default_flux"] = store.get("biomass_default_flux", list())
+    model.objective = reaction
+    try:
+        solution = model.optimize()
+        flux = solution.fluxes[reaction.id]
+    except Infeasible:
+        flux = np.nan
+    store["biomass_default_flux"].append(flux)
+    assert flux > 0.0
 
 
-def test_biomass_precursors_default_production(model, biomass_reactions, store):
+@pytest.mark.parametrize("reaction", biomass_reactions, ids=biomass_ids)
+def test_biomass_precursors_default_production(read_only_model, reaction,
+                                               store):
     """Expect production of all biomass precursors in default medium."""
-    store["default_blocked_precursors"] = list()
-    for rxn in biomass_reactions:
-        store["default_blocked_precursors"].append(
-            [met.id
-             for met in biomass.find_blocked_biomass_precursors(rxn, model)])
-    for rxn, blocked in zip(biomass_reactions,
-                            store["default_blocked_precursors"]):
-        assert len(blocked) == 0,\
-            "{}'s following precursors cannot be produced: {}"\
-            "".format(rxn.id, ", ".join(blocked))
+    store["default_blocked_precursors"] = store.get(
+        "default_blocked_precursors", list())
+    blocked = [
+        met.id for met in biomass.find_blocked_biomass_precursors(
+            reaction, read_only_model)]
+    store["default_blocked_precursors"].append(blocked)
+    assert len(blocked) == 0, \
+        "{}'s following precursors cannot be produced: {}" \
+        "".format(reaction.id, ", ".join(blocked))
 
 
-def test_biomass_precursors_open_production(model, biomass_reactions, store):
+@pytest.mark.parametrize("reaction", biomass_reactions, ids=biomass_ids)
+def test_biomass_precursors_open_production(model, reaction, store):
     """Expect precursor production in complete medium."""
-    store["open_blocked_precursors"] = list()
+    store["open_blocked_precursors"] = store.get(
+        "open_blocked_precursors", list())
     for exchange in model.exchanges:
         exchange.bounds = (-1000, 1000)
-    for rxn in biomass_reactions:
-        store["open_blocked_precursors"].append(
-            [met.id
-             for met in biomass.find_blocked_biomass_precursors(rxn, model)])
-    for rxn, blocked in zip(biomass_reactions,
-                            store["open_blocked_precursors"]):
-        assert len(blocked) == 0, \
-            "{}'s following precursors cannot be produced: {}" \
-            "".format(rxn.id, ", ".join(blocked))
+    blocked = [
+        met.id for met in biomass.find_blocked_biomass_precursors(
+            reaction, model)]
+    store["open_blocked_precursors"].append(blocked)
+    assert len(blocked) == 0, \
+        "{}'s following precursors cannot be produced: {}" \
+        "".format(reaction.id, ", ".join(blocked))
