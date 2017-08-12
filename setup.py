5ecb252e5a98d10cec1bd95982653aedb3c7e474
Midnighter <midnighter@posteo.net>
2017-08-12 01:50:42 -0400

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ test_requirements = [
 
 setup(
     name="memote",
-    version="0.3.1",
+    version="0.3.2",
     description="Genome-scale metabolic model test suite.",
     long_description=readme + "\n\n" + history,
     author="Moritz E. Beber",
