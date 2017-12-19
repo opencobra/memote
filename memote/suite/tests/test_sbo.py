1ac7410ffa2298f16885b1e09a4b4181a46b1ccb
ChristianLieven <clie@biosustain.dtu.dk>
2017-12-19 12:03:23 +0100

    feat: add more tests for specific SBO terms

--- memote/suite/tests/test_sbo.py
+++ memote/suite/tests/test_sbo.py
@@ -21,6 +21,7 @@ from __future__ import absolute_import, division
 
 import memote.support.basic as basic
 import memote.support.helpers as helpers
+import memote.support.sbo as sbo
 
 import memote.support.annotation as annotation
 from memote.utils import annotate, truncate, get_ids, wrapper
@@ -100,12 +101,13 @@ def test_metabolic_reaction_sbo_presence(read_only_model):
     """
     ann = test_metabolic_reaction_sbo_presence.annotation
     ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
-        basic.find_pure_metabolic_reactions(read_only_model)) "SBO:0000176"))
+        basic.find_pure_metabolic_reactions(read_only_model), "SBO:0000176"))
     ann["metric"] = len(ann["data"]) / len(
     basic.find_pure_metabolic_reactions(read_only_model))
     ann["message"] = wrapper.fill(
-        """A total of {} metabolic reactions ({:.2%}) lack annotation with the
-        SBO term "SBO:0000176" for 'biochemical reaction': {}""".format(
+        """A total of {} metabolic reactions ({:.2%} of all purely metabolic
+        reactions) lack annotation with the SBO term "SBO:0000176" for
+        'biochemical reaction': {}""".format(
             len(ann["data"]), ann["metric"], truncate(ann["data"])
         ))
     assert len(ann["data"]) == 0, ann["message"]
@@ -122,12 +124,184 @@ def test_transport_reaction_sbo_presence(read_only_model):
     """
     ann = test_transport_reaction_sbo_presence.annotation
     ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
-        helpers.find_transport_reactions(read_only_model)) "SBO:0000185"))
+        helpers.find_transport_reactions(read_only_model), "SBO:0000185"))
     ann["metric"] = len(ann["data"]) / len(
     basic.find_transport_reactions(read_only_model))
     ann["message"] = wrapper.fill(
-        """A total of {} transport reactions ({:.2%}) lack annotation with the
-        SBO term "SBO:0000185" for 'transport reaction': {}""".format(
+        """A total of {} metabolic reactions ({:.2%} of all transport
+        reactions) lack annotation with the SBO term "SBO:0000185" for
+        'biochemical reaction': {}""".format(
+            len(ann["data"]), ann["metric"], truncate(ann["data"])
+        ))
+    assert len(ann["data"]) == 0, ann["message"]
+
+
+@annotate(title="Metabolites without SBO:0000247", type="length")
+def test_metabolite_sbo_presence(read_only_model):
+    """Expect all metabolites to be annotated with SBO:0000247.
+
+    SBO:0000247 represents the term 'simple chemical'. Every metabolite should
+    be annotated with this.
+    """
+    ann = test_metabolite_sbo_presence.annotation
+    ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
+        read_only_model.metabolites, "SBO:0000247"))
+    ann["metric"] = len(ann["data"]) / len(read_only_model.metabolites)
+    ann["message"] = wrapper.fill(
+        """A total of {} transport reactions ({:.2%} of all metabolites) lack
+        annotation with the SBO term "SBO:0000247" for
+        'simple chemical': {}""".format(
+            len(ann["data"]), ann["metric"], truncate(ann["data"])
+        ))
+    assert len(ann["data"]) == 0, ann["message"]
+
+
+@annotate(title="Genes without SBO:0000243", type="length")
+def test_gene_sbo_presence(read_only_model):
+    """Expect all genes to be annotated with SBO:0000243.
+
+    SBO:0000243 represents the term 'gene'. Every gene should
+    be annotated with this.
+    """
+    ann = test_gene_sbo_presence.annotation
+    ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
+        read_only_model.genes, "SBO:0000243"))
+    ann["metric"] = len(ann["data"]) / len(read_only_model.genes)
+    ann["message"] = wrapper.fill(
+        """A total of {} genes ({:.2%} of all genes) lack
+        annotation with the SBO term "SBO:0000243" for
+        'gene': {}""".format(
+            len(ann["data"]), ann["metric"], truncate(ann["data"])
+        ))
+    assert len(ann["data"]) == 0, ann["message"]
+
+
+@annotate(title="Exchange reactions without SBO:0000627", type="length")
+def test_exchange_sbo_presence(read_only_model):
+    """Expect all exchange reactions to be annotated with SBO:0000627.
+
+    SBO:0000627 represents the term 'exchange reaction'. The Systems Biology
+    Ontology defines an exchange reaction as follows: 'A modeling process to
+    provide matter influx or efflux to a model, for example to replenish a
+    metabolic network with raw materials (eg carbon / energy sources). Such
+    reactions are conceptual, created solely for modeling purposes, and do not
+    have a  physical correspondence. Exchange reactions, often represented as
+    'R_EX_', can operate in the negative (uptake) direction or positive
+    (secretion) direction. By convention, a negative flux through an exchange
+    reaction represents uptake of the corresponding metabolite, and a positive
+    flux represent discharge.' Every exchange reaction should be annotated with
+    this. Exchange reactions differ from demand reactions in that the
+    metabolites are removed from or added to the extracellular
+    environment only.
+    """
+    ann = test_gene_sbo_presence.annotation
+    ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
+        helpers.find_exchange_rxns(read_only_model), "SBO:0000627"))
+    ann["metric"] = len(ann["data"]) / len(
+        helpers.find_exchange_rxns(read_only_model))
+    ann["message"] = wrapper.fill(
+        """A total of {} exchange reactions ({:.2%} of all exchange reactions)
+        lack annotation with the SBO term "SBO:0000627" for
+        'exchange reaction': {}""".format(
+            len(ann["data"]), ann["metric"], truncate(ann["data"])
+        ))
+    assert len(ann["data"]) == 0, ann["message"]
+
+
+@annotate(title="Demand reactions without SBO:0000628", type="length")
+def test_demand_sbo_presence(read_only_model):
+    """Expect all demand reactions to be annotated with SBO:0000627.
+
+    SBO:0000628 represents the term 'demand reaction'. The Systems Biology
+    Ontology defines an exchange reaction as follows: 'A modeling process
+    analogous to exchange reaction, but which operates upon "internal"
+    metabolites. Metabolites that are consumed by these reactions are assumed
+    to be used in intra-cellular processes that are not part of the model.
+    Demand reactions, often represented 'R_DM_', can also deliver metabolites
+    (from intra-cellular processes that are not considered in the model).'
+    Every demand reaction should be annotated with
+    this. Demand reactions differ from exchange reactions in that the
+    metabolites are not removed from the extracellular environment, but from
+    any of the organism's compartments. Demand reactions differ from sink
+    reactions in that they are designated as irreversible.
+    """
+    ann = test_demand_sbo_presence.annotation
+    ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
+        helpers.find_demand_reactions(read_only_model), "SBO:0000628"))
+    ann["metric"] = len(ann["data"]) / len(
+        helpers.find_demand_reactions(read_only_model))
+    ann["message"] = wrapper.fill(
+        """A total of {} genes ({:.2%} of all demand reactions) lack
+        annotation with the SBO term "SBO:0000628" for
+        'demand reaction': {}""".format(
+            len(ann["data"]), ann["metric"], truncate(ann["data"])
+        ))
+    assert len(ann["data"]) == 0, ann["message"]
+
+
+@annotate(title="Sink reactions without SBO:0000632", type="length")
+def test_sink_sbo_presence(read_only_model):
+    """Expect all sink reactions to be annotated with SBO:0000632.
+
+    SBO:0000632 represents the term 'sink reaction'. The Systems Biology
+    Ontology defines an exchange reaction as follows: 'A modeling process to
+    provide matter influx or efflux to a model, for example to replenish a
+    metabolic network with raw materials (eg carbon / energy sources). Such
+    reactions are conceptual, created solely for modeling purposes, and do not
+    have a physical correspondence. Unlike the analogous demand (SBO:....)
+    reactions, which are usually designated as irreversible, sink reactions
+    always represent a reversible uptake/secretion processes, and act as a
+    metabolite source with no cost to the cell. Sink reactions, also referred
+    to as R_SINK_, are generally used for compounds that are metabolized by
+    the cell but are produced by non-metabolic, un-modeled cellular processes.'
+    Every sink reaction should be annotated with
+    this. Sink reactions differ from exchange reactions in that the metabolites
+    are not removed from the extracellular environment, but from any of the
+    organism's compartments.
+    """
+    ann = test_sink_sbo_presence.annotation
+    ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
+        helpers.find_sink_reactions(read_only_model), "SBO:0000632"))
+    ann["metric"] = len(ann["data"]) / len(
+        helpers.find_sink_reactions(read_only_model))
+    ann["message"] = wrapper.fill(
+        """A total of {} genes ({:.2%} of all sink reactions) lack
+        annotation with the SBO term "SBO:0000632" for
+        'sink reaction': {}""".format(
+            len(ann["data"]), ann["metric"], truncate(ann["data"])
+        ))
+    assert len(ann["data"]) == 0, ann["message"]
+
+
+@annotate(title="Biomass reactions without SBO:0000629", type="length")
+def test_biomass_sbo_presence(read_only_model):
+    """Expect all biomass reactions to be annotated with SBO:0000629.
+
+    SBO:0000629 represents the term 'biomass production'. The Systems Biology
+    Ontology defines an exchange reaction as follows: 'Biomass production,
+    often represented 'R_BIOMASS_', is usually the optimization target reaction
+    of constraint-based models, and can consume multiple reactants to produce
+    multiple products. It is also assumed that parts of the reactants are also
+    consumed in unrepresented processes and hence products do not have to
+    reflect all the atom composition of the reactants. Formulation of a
+    biomass production process entails definition of the macromolecular
+    content (eg. cellular protein fraction), metabolic constitution of
+    each fraction (eg. amino acids), and subsequently the atomic composition
+    (eg. nitrogen atoms). More complex biomass functions can additionally
+    incorporate details of essential vitamins and cofactors required for
+    growth.'
+    Every reaction representing the biomass production should be annotated with
+    this.
+    """
+    ann = test_biomass_sbo_presence.annotation
+    ann["data"] = get_ids(sbo.check_component_for_specific_sbo_term(
+        helpers.find_biomass_reaction(read_only_model), "SBO:0000629"))
+    ann["metric"] = len(ann["data"]) / len(
+        helpers.find_biomass_reaction(read_only_model))
+    ann["message"] = wrapper.fill(
+        """A total of {} biomass reactions ({:.2%} of all biomass reactions)
+        lack annotation with the SBO term "SBO:0000629" for
+        'biomass production': {}""".format(
             len(ann["data"]), ann["metric"], truncate(ann["data"])
         ))
     assert len(ann["data"]) == 0, ann["message"]
