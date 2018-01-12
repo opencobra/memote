47430026c75a4af4b0362f93bc800aa0dc718082
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-01-12 10:24:58 +0100

    feat: enable inlining via gulp

---
+++ memote-report-app/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var inline = require('gulp-inline');
+  // , uglify = require('gulp-uglify')
+  // , minifyCss = require('gulp-minify-css')
+  // , autoprefixer = require('gulp-autoprefixer');
+
+function inlining() {
+  return gulp.src('dist/index.html')
+    .pipe(inline({
+      base: 'dist/'
+      // js: uglify,
+      // css: [minifyCss, autoprefixer({ browsers:['last 2 versions'] })],
+      // disabledTypes: ['svg', 'img', 'js'], // Only inline css files
+      // ignore: ['./css/do-not-inline-me.css']
+    }))
+    .pipe(gulp.dest('build/'));
+}
+
+gulp.task('inline', inlining);
