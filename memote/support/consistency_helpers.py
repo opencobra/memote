efbf79fdafe49ec1e4bdde6206115b6cc524e143
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-02-12 17:27:41 +0100

    style: more flake8 appeasement

--- memote/support/consistency_helpers.py
+++ memote/support/consistency_helpers.py
@@ -136,7 +136,6 @@ def rank(A, atol=1e-13, rtol=0):
         provide the option of the absolute tolerance.
 
     """
-
     A = np.atleast_2d(A)
     s = svd(A, compute_uv=False)
     tol = max(atol, rtol * s[0])
@@ -199,7 +198,6 @@ def nullspace_basis(A, atol=1e-13, rtol=0):
         zero.
 
     """
-
     A = np.atleast_2d(A)
     u, s, vh = svd(A)
     tol = max(atol, rtol * s[0])
