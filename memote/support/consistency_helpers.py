af8e4f5a26250f1a7f7bd88bd2a626fe7603aeb4
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-02-16 14:07:40 +0100

    feat: add rank and nullspace_basis functions

--- memote/support/consistency_helpers.py
+++ memote/support/consistency_helpers.py
@@ -95,6 +95,49 @@ def stoichiometry_matrix(metabolites, reactions):
     return matrix, met_index, rxn_index
 
 
+def rank(A, atol=1e-13, rtol=0):
+    """Estimate the rank (i.e. the dimension of the column space) of a matrix.
+
+    The algorithm used by this function is based on the singular value
+    decomposition of `A`.
+
+    Parameters
+    ----------
+    A : ndarray
+        A should be at most 2-D.  A 1-D array with length n will be treated
+        as a 2-D with shape (1, n)
+    atol : float
+        The absolute tolerance for a zero singular value.  Singular values
+        smaller than `atol` are considered to be zero.
+    rtol : float
+        The relative tolerance.  Singular values less than rtol*smax are
+        considered to be zero, where smax is the largest singular value.
+
+    If both `atol` and `rtol` are positive, the combined tolerance is the
+    maximum of the two; that is::
+        tol = max(atol, rtol * smax)
+    Singular values smaller than `tol` are considered to be zero.
+
+    Returns
+    -------
+    rank : int
+        The estimated rank of the matrix.
+
+    See Also
+    --------
+    numpy.linalg.matrix_rank
+        matrix_rank is basically the same as this function, but it does not
+        provide the option of the absolute tolerance.
+
+    """
+
+    A = np.atleast_2d(A)
+    s = svd(A, compute_uv=False)
+    tol = max(atol, rtol * s[0])
+    rank = int((s >= tol).sum())
+    return rank
+
+
 def nullspace(matrix, atol=1e-13, rtol=0.0):
     """
     Compute the nullspace of a 2D `numpy.array`.
@@ -109,6 +152,52 @@ def nullspace(matrix, atol=1e-13, rtol=0.0):
     _, s, vh = svd(matrix)
     tol = max(atol, rtol * s[0])
     return np.compress(s < tol, vh, axis=0).T
+
+
+def nullspace_basis(A, atol=1e-13, rtol=0):
+    """Compute an approximate basis for the nullspace of A.
+
+    The algorithm used by this function is based on the singular value
+    decomposition of `A`.
+
+    Parameters
+    ----------
+    A : ndarray
+        A should be at most 2-D.  A 1-D array with length k will be treated
+        as a 2-D with shape (1, k)
+    atol : float
+        The absolute tolerance for a zero singular value.  Singular values
+        smaller than `atol` are considered to be zero.
+    rtol : float
+        The relative tolerance.  Singular values less than rtol*smax are
+        considered to be zero, where smax is the largest singular value.
+
+    If both `atol` and `rtol` are positive, the combined tolerance is the
+    maximum of the two; that is::
+        tol = max(atol, rtol * smax)
+    Singular values smaller than `tol` are considered to be zero.
+
+    Returns
+    -------
+    ns : ndarray
+        If `A` is an array with shape (m, k), then `ns` will be an array
+        with shape (k, n), where n is the estimated dimension of the
+        nullspace of `A`.  The columns of `ns` are a basis for the
+        nullspace; each element in numpy.dot(A, ns) will be approximately
+        zero.
+
+    Notes
+    -----
+    Adapted from:
+    https://scipy.github.io/old-wiki/pages/Cookbook/RankNullspace.html
+    """
+
+    A = np.atleast_2d(A)
+    u, s, vh = svd(A)
+    tol = max(atol, rtol * s[0])
+    nnz = (s >= tol).sum()
+    ns = vh[nnz:].conj().T
+    return ns
 
 
 def get_interface(model):
