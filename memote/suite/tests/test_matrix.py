ab6c4805277ac5994007b2beba5697b25346818f
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-02-16 11:58:40 +0100

    feat: add matrix tests (#354)

---
+++ memote/suite/tests/test_matrix.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Novo Nordisk Foundation Center for Biosustainability,
+# Technical University of Denmark.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for matrix properties performed on an instance of ``cobra.Model``."""
+
+from __future__ import absolute_import, division
+
+import memote.support.matrix as matrix
+from memote.utils import annotate, wrapper
+
+
+@annotate(title="Ratio between largest and smallest non-zero coefficients",
+          type="percent")
+def test_absolute_extreme_coefficient_ratio(model):
+    """
+    Show ratio of the absolute largest and smallest non-zero coefficients.
+
+    This test will return the absolute largest and smallest, non-zero
+    coefficients from the S-Matrix. A large ratio of these values may point to
+    potential numerical issues when trying to solve the underlying system of
+    equations.
+
+    To pass this test the ratio should not exceed 10^9. This threshold has
+    been selected based on experience, and is likely to be adapted when more
+    data on solver performance becomes available.
+    """
+    ann = test_absolute_extreme_coefficient_ratio.annotation
+    ann["data"] = matrix.absolute_extreme_coefficient_ratio(model)
+    ann["metric"] = ann["data"][0] / ann["data"][1]
+    ann["message"] = wrapper.fill(
+        """The ratio of the absolute highest coefficient {} and the lowest,
+        non-zero coefficient {} is: {}.""".format(
+            ann["data"][0], ann["data"][1], ann['metric']))
+    assert ann["metric"] < 1e9, ann["message"]
+
+
+@annotate(title="Number of independent conservation relations in model",
+          type="number")
+def test_number_independent_conservation_relations(model):
+    """
+    Show number of independent conservation relations in the model.
+
+    This test will return the number of conservation relations, i.e.
+    conservation pools through the left null space of the S-Matrix.
+
+    This test is not scored, as the dimension of the left null space
+    depends on the S-Matrix constructed, which is system-specific.
+    """
+    ann = test_number_independent_conservation_relations.annotation
+    ann["data"] = matrix.number_independent_conservation_relations(model)
+    ann["message"] = wrapper.fill(
+        """The number of independent conservation relations is {}.""".format(
+            ann["data"]))
+
+
+@annotate(title="Number of steady-state flux solution vectors", type="number")
+def test_number_steady_state_flux_solutions(model):
+    """
+    Show number of independent steady-state flux solution vectors for model.
+
+    This test will return the number of steady-state flux solution vectors
+    through the null space of the S-Matrix.
+
+    This test is not scored, as the dimension of the null space depends on the
+    S-Matrix constructed, which is system-specific.
+    """
+    ann = test_number_steady_state_flux_solutions.annotation
+    ann["data"] = matrix.number_steady_state_flux_solutions(model)
+    ann["message"] = wrapper.fill(
+        """The number of independent steady-state flux solution vectors is {}.
+        """.format(ann["data"]))
+
+
+@annotate(title="Rank of S-Matrix", type="number")
+def test_matrix_rank(model):
+    """
+    Show rank of the S-Matrix.
+
+    This test will return the rank of the S-Matrix of the model.
+
+    This test is not scored, as the rank depends on the S-Matrix constructed,
+    which is system-specific.
+    """
+    ann = test_matrix_rank.annotation
+    ann["data"] = matrix.matrix_rank(model)
+    ann["message"] = wrapper.fill(
+        """The rank of the S-Matrix is {}.""".format(ann["data"]))
+
+
+@annotate(title="Degrees of freedom of S-Matrix", type="number")
+def test_degrees_of_freedom(model):
+    """
+    Show degrees of freedom of the S-Matrix.
+
+    This test will return the degrees of freedom, i.e. "free variables" of the
+    S-Matrix.
+
+    This test is not scored, as the degrees of freedom depends on S-Matrix
+    constructed, which is system-specific.
+    """
+    ann = test_degrees_of_freedom.annotation
+    ann["data"] = matrix.degrees_of_freedom(model)
+    ann["message"] = wrapper.fill(
+        """The degrees of freedom of the S-Matrix is {}.""".format(ann["data"]))
---
+++ memote/support/matrix.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 Novo Nordisk Foundation Center for Biosustainability,
+# Technical University of Denmark.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Supporting functions checking the matrix condition of the model object."""
+
+from __future__ import absolute_import
+
+import logging
+import numpy as np
+import memote.support.consistency_helpers as con_helpers
+
+LOGGER = logging.getLogger(__name__)
+
+
+def absolute_extreme_coefficient_ratio(model):
+    """Return the absolute max and absolute non-zero min coefficients."""
+    # S-Matrix with absolute values:
+    s_matrix, _, _ = con_helpers.stoichiometry_matrix(
+        model.metabolites, model.reactions
+    )
+    abs_matrix = np.absolute(s_matrix)
+
+    absolute_max_coef = np.amax(abs_matrix)
+    absolute_non_zero_min_coef = abs_matrix[abs_matrix > 0].min()
+
+    return (absolute_max_coef, absolute_non_zero_min_coef)
+
+
+def number_independent_conservation_relations(model):
+    """Return the amount of conserved metabolic pools."""
+    s_matrix, _, _ = con_helpers.stoichiometry_matrix(
+        model.metabolites, model.reactions
+    )
+    ln_matrix = con_helpers.nullspace_basis(s_matrix.T)
+    return ln_matrix.shape[1]
+
+
+def number_steady_state_flux_solutions(model):
+    """Return the amount of steady-state flux solutions of this model."""
+    s_matrix, _, _ = con_helpers.stoichiometry_matrix(
+        model.metabolites, model.reactions
+    )
+    n_matrix = con_helpers.nullspace_basis(s_matrix)
+    return n_matrix.shape[1]
+
+
+def matrix_rank(model):
+    """Return the rank of the model's stoichiometric matrix."""
+    s_matrix, _, _ = con_helpers.stoichiometry_matrix(
+        model.metabolites, model.reactions
+    )
+    return con_helpers.rank(s_matrix)
+
+
+def degrees_of_freedom(model):
+    """
+    Return the degrees of freedom, i.e. number of "free variables".
+
+    Notes:
+    ------
+    This specifically refers to the dimensionality of the right nullspace
+    of the S matrix, as dim(Null(S)) corresponds directly to the number of
+    free variables in the system [1]. The forumla used calculates this using
+    the rank-nullity theorem [2].
+
+    References:
+    -----------
+    .. [1] Fukuda, K. & Terlaky, T. Criss-cross methods: A fresh view on
+           pivot algorithms. Mathematical Programming 79, 369-395 (1997).
+
+    .. [2] Alama, J. The Rank+Nullity Theorem. Formalized Mathematics 15,
+           (2007).
+
+    """
+    s_matrix, _, _ = con_helpers.stoichiometry_matrix(
+        model.metabolites, model.reactions
+    )
+    return s_matrix.shape[1] - matrix_rank(model)
