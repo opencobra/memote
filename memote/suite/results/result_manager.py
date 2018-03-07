df5307961807bf66ef24fa94486bcd64cbff98f5
ChristianLieven <clie@biosustain.dtu.dk>
2018-03-07 13:44:21 +0100

    fix: fix TypeError associated with memote new

--- memote/suite/results/result_manager.py
+++ memote/suite/results/result_manager.py
@@ -76,13 +76,14 @@ class ResultManager(object):
         if env_info:
             self.add_environment(result.meta)
         if pretty:
-            kwargs = dict(sort_keys=True, indent=2, separators=(",", ": "))
+            kwargs = dict(sort_keys=True, indent=2, separators=(",", ": "), ensure_ascii=True)
         else:
-            kwargs = dict(sort_keys=False, indent=None, separators=(",", ":"))
+            kwargs = dict(sort_keys=False, indent=None, separators=(",", ":"), ensure_ascii=True)
         LOGGER.info("Storing result in '%s'.", filename)
         with open(filename, "w", encoding="utf-8") as file_handle:
             try:
-                return json.dump(result, file_handle, **kwargs)
+                return file_handle.write(
+                    unicode(json.dumps(result, **kwargs)))
             except TypeError as error:
                 log_json_incompatible_types(result)
                 raise_with_traceback(error)
