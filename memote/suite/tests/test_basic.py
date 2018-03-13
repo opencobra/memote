263b31e58e26f320763f4e6433be4481449ead13
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-03-13 10:08:37 +0100

    fix: return set of tuples
    test_find_constrained_transport_reactions now returns a set of tuples (id, lower_bound, upper_bound) instead of set of ids

--- memote/suite/tests/test_basic.py
+++ memote/suite/tests/test_basic.py
@@ -346,7 +346,7 @@ def test_find_constrained_transport_reactions(read_only_model):
     """
     ann = test_find_constrained_transport_reactions.annotation
     transporters = set(helpers.find_transport_reactions(read_only_model))
-    ann["data"] = get_ids(set(
+    ann["data"] = get_ids_and_bounds(set(
         [rxn for rxn in transporters if basic.is_constrained_reaction(rxn)]))
     ann["metric"] = len(ann["data"]) / len(transporters)
     ann["message"] = wrapper.fill(
