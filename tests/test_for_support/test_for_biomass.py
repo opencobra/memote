ec210bec40921f383b5caed07c5983f9f62536b5
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-03-22 10:31:01 +0100

    fix: fix test

--- tests/test_for_support/test_for_biomass.py
+++ tests/test_for_support/test_for_biomass.py
@@ -421,7 +421,7 @@ def test_fast_growth_default(model, boolean):
 
 @pytest.mark.parametrize("model, number", [
     ("only_direct_mets", 3),
-    ("direct_met_false_positive_single_compartment", 0),
+    ("direct_met_false_positive_single_compartment", 3),
     ("precursors_producing", 0)
 ], indirect=["model"])
 def test_find_direct_metabolites(model, number):
