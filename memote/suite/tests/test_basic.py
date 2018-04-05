8bb8e520a8b2a4f5e3de8207ef5f47a8738dcb6e
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-04-05 17:10:54 +0200

    docs: extend extended summary

--- memote/suite/tests/test_basic.py
+++ memote/suite/tests/test_basic.py
@@ -324,6 +324,26 @@ def test_find_transport_reactions(read_only_model):
     to another, are present in the model, as at least one transport reaction
     must be present for cells to take up food and nutrients and/or excrete 
     waste.
+
+    A transport reaction is defined as follows:
+    1. It contains metabolites from at least 2 compartments and
+    2. at least 1 metabolite undergoes no chemical reaction, i.e.,
+    the formula stays the same on both sides of the equation.
+
+    A notable exception is transport via PTS, which is defined as follows:
+    1. The transported metabolite(s) come from the ``e`` compartment into the
+    ``c`` compartment and
+    2. the metabolite in the ``e`` compartment enters into the ``c``
+    compartment through the exchange of a phosphate.
+
+    An example of tranport via PTS would be
+    pep(c) + glucose(e) -> glucose-6-phosphate(c) + pyr(c)
+
+    Transport via PTS is only detected when a formula field exists for all
+    metabolites in a particular reaction. If this is not the case, transport
+    reactions are identified through annotations, which cannot detect transport
+    via PTS.
+
     """
     ann = test_find_transport_reactions.annotation
     ann["data"] = get_ids(helpers.find_transport_reactions(read_only_model))
