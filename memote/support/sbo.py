07530d16c3c77cf426d163f7d0c1c4a2a78a8ece
Siddharth Chauhan <github.chauhan.siddharth@gmail.com>
2018-04-10 11:02:07 +0200

    refactor: change function name

--- memote/support/sbo.py
+++ memote/support/sbo.py
@@ -45,9 +45,9 @@ def find_components_without_sbo_terms(model, components):
             elem.annotation is None or 'SBO' not in elem.annotation]
 
 
-def check_component_for_specific_sbo_term(items, term):
+def check_component_for_specific_sbo_terms(items, term):
     """
-    Identify model components that lack a specific SBO term.
+    Identify model components that lack a specific SBO term(s).
 
     Parameters
     ----------
