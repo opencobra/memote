cd027c8f56e51ba678e1f363cee68307b267b397
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2018-12-10 12:29:28 +0100

    refactor: disable name matching

--- memote/support/thermodynamics.py
+++ memote/support/thermodynamics.py
@@ -64,7 +64,7 @@ def get_smallest_compound_id(compounds_identifiers):
                key=lambda c: int(c[1:]))
 
 
-def map_metabolite2kegg(metabolite, threshold=0.85):
+def map_metabolite2kegg(metabolite):
     """
     Return a KEGG compound identifier for the metabolite if it exists.
 
@@ -79,13 +79,14 @@ def map_metabolite2kegg(metabolite, threshold=0.85):
     Parameters
     ----------
     metabolite : cobra.Metabolite
-    threshold : float, optional
-        Cut off for the name matching score. Can be between 0 and 1 with 1
-        being the stricter side.
+        The metabolite to be mapped to its KEGG compound identifier.
 
     Returns
     -------
+    None
+        If the metabolite could not be mapped.
     str
+        The smallest KEGG compound identifier that was found.
 
     """
     logger.debug("Looking for KEGG compound identifier for %s.", metabolite.id)
@@ -132,12 +133,16 @@ def translate_reaction(reaction, metabolite_mapping):
     Parameters
     ----------
     reaction : cobra.Reaction
+        The reaction whose metabolites are to be translated.
     metabolite_mapping : dict
+        An existing mapping from cobra.Metabolite to KEGG compound identifier
+        that may already contain the metabolites in question or will have to be
+        extended.
 
     Returns
     -------
     dict
-        The stoichiometry of a reaction given as a mapping from metabolite
+        The stoichiometry of the reaction given as a mapping from metabolite
         KEGG identifier to coefficient.
 
     """
@@ -150,54 +155,36 @@ def translate_reaction(reaction, metabolite_mapping):
         if kegg_id is None:
             continue
         stoichiometry[kegg_id] += coef
-    return stoichiometry
+    return dict(stoichiometry)
 
 
-def find_incorrect_thermodynamic_reversibility(reactions, ln_gamma=3):
+def find_thermodynamic_reversibility_index(reactions):
     u"""
-    Return reactions whose reversibilities do not agree with thermodynamics.
+    Return the reversibility index of the given reactions.
 
-    This function checks if the reversibility attribute of each reaction
-    in a list of cobrapy reactions agrees with a thermodynamics-based
-    calculation of the reversibility. To determine reversibility we calculate
+    To determine the reversibility index, we calculate
     the reversibility index ln_gamma (see [1]_ section 3.5) of each reaction
-    using the eQuilibrator API [2]_. The default cutoff for ln_gamma
-    "corresponds to allowing concentrations to span three orders of magnitude
-    around 100 μM (~3 μM—3 mM)" at "pH = 7, I = 0.1 M and T = 298 K" (see [1]_
-    supplement section 3). Here pH denotes the negative base 10 logarithm of
-    the activity of the hydrogen ion i.e. a measure of acidity/ basicity of an
-    aqueous solution, I denotes the molar ionic strength which is a measure of
-    the concentration of ions in a solution, lastly, T denotes the
-    thermodynamic temperature also called absolute temperature measured in
-    Kelvin.
+    using the eQuilibrator API [2]_.
 
     Parameters
     ----------
-        reactions: list of cobra.Reactions
-            A list of reactions to be checked for agreement with
-            thermodynamics-based calculations of reversibility.
-        ln_gamma: integer
-            Log-scale, symmetric range of metabolite concentrations around the
-            assumed average of 100 µM. A threshold of 3 means that a
-            reaction is considered irreversible if the concentration of an
-            individual metabolite would have to change more than three orders
-            of magnitude i.e. from 3 µM to 3 mM to reverse the direction of
-            flux.
+        reactions: list of cobra.Reaction
+            A list of reactions for which to calculate the reversibility index.
 
     Returns
     -------
-        incorrect_reversibility: list of cobra.Reactions
-            A list of reactions whose reversibility does not agree
-            with thermodynamic calculation.
-        incomplete_mapping: list of cobra.Reactions
+    tuple
+        list of cobra.Reaction, index pairs
+            A list of pairs of reactions and their reversibility indexes.
+        list of cobra.Reaction
             A list of reactions which contain at least one metabolite that
-            could not be mapped to KEGG on the basis of its annotation or name.
-        problematic_calculation: list of cobra.Reactions
+            could not be mapped to KEGG on the basis of its annotation.
+        list of cobra.Reaction
             A list of reactions for which it is not possible to calculate the
-            standard change in Gibbs potential. Reasons of failure include that
-            participating metabolites cannot be broken down with the group
-            contribution method.
-        unbalanced: list of cobra.Reactions
+            standard change in Gibbs free energy potential. Reasons of failure
+            include that participating metabolites cannot be broken down with
+            the group contribution method.
+        list of cobra.Reaction
             A list of reactions that are not chemically or redox balanced.
 
 
@@ -208,12 +195,12 @@ def find_incorrect_thermodynamic_reversibility(reactions, ln_gamma=3):
            reactions that combines accuracy and coverage, Bioinformatics,
            Volume 28, Issue 15, 1 August 2012, Pages 2037–2044,
            https://doi.org/10.1093/bioinformatics/bts317
-    .. [2] https://gitlab.com/elad.noor/equilibrator-api/tree/master
+    .. [2] https://pypi.org/project/equilibrator-api/
 
     """
     incomplete_mapping = []
     problematic_calculation = []
-    incorrect_reversibility = []
+    reversibility_indexes = []
     unbalanced = []
     metabolite_mapping = {}
 
@@ -233,18 +220,14 @@ def find_incorrect_thermodynamic_reversibility(reactions, ln_gamma=3):
         if eq_rxn.check_full_reaction_balancing():
             try:
                 ln_rev_index = eq_rxn.reversibility_index()
-            # TODO: Which exceptions can we expect here?
+            # TODO (Moritz Beber): Which exceptions can we expect here?
             except Exception:
                 problematic_calculation.append(rxn)
                 continue
-            if (ln_rev_index < ln_gamma) != rxn.reversibility:
-                incorrect_reversibility.append(rxn)
-            else:
-                continue
+            reversibility_indexes.append((rxn, ln_rev_index))
         else:
             unbalanced.append(rxn)
-
     return (
-        incorrect_reversibility, incomplete_mapping, problematic_calculation,
+        reversibility_indexes, incomplete_mapping, problematic_calculation,
         unbalanced
     )
