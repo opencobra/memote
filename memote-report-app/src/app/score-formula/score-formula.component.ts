0814ed54e209c63eb68b4b920ae9890bab328f9a
ChristianLieven <clie@biosustain.dtu.dk>
2019-01-10 12:12:51 +0100

    feat: add summation-style subscores to the scored report side.

--- memote-report-app/src/app/score-formula/score-formula.component.ts
+++ memote-report-app/src/app/score-formula/score-formula.component.ts
@@ -8,11 +8,46 @@ import { ReportDataService } from './../report-data.service';
   encapsulation: ViewEncapsulation.None
 })
 export class ScoreFormulaComponent implements OnInit {
-  @Input() testId: string;
+  @Input() position: string;
+  scoreType = 'Total Score';
+  modelScore: string[] = [];
 
   constructor(private data: ReportDataService) {
   }
 
   ngOnInit() {
+    this.obtainScore();
   }
+
+  obtainScore() {
+    switch (this.data.reportType) {
+      case 'snapshot': {
+        if (this.position !== 'Total Score') {
+          for (const sectionScoreObject of this.data.score.sections) {
+            if (sectionScoreObject.section === this.position) {
+              this.modelScore.push(sectionScoreObject.score);
+            }
+            }
+            this.scoreType = 'Sub Total';
+            return this.modelScore;
+          }
+        return this.modelScore === this.data.score.total_score;
+      }
+      case 'diff': {
+        if (this.position !== 'Total Score') {
+          for (const sectionScoreObject of this.data.score.sections.diff) {
+            if (sectionScoreObject.section === this.position) {
+              this.modelScore.push(sectionScoreObject.score);
+            }
+            }
+            this.scoreType = 'Sub Total';
+            return this.modelScore;
+          }
+        for (const sectionScoreObject of this.data.score.total_score.diff) {
+            this.modelScore.push(sectionScoreObject.total_score);
+          }
+      }
+  }
+  }
+
 }
