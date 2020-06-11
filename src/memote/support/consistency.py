fix: delegate tolerance to nullspace calculation

src/memote/support/consistency.py
@@ -217,10 +217,11 @@ def find_inconsistent_min_stoichiometry(model, atol=None):
     metabolites = sorted(internal_mets, key=get_id)
     stoich, met_index, rxn_index = con_helpers.stoichiometry_matrix(
         metabolites, reactions)
-    left_ns = con_helpers.nullspace(stoich.T)
-    # deal with numerical instabilities
-    left_ns[np.abs(left_ns) < atol] = 0.0
-    LOGGER.info("nullspace has dimension %d", left_ns.shape[1])
+    left_ns = con_helpers.nullspace(stoich.T, atol)
+    if len(left_ns.shape) > 1:
+        LOGGER.info(
+            "nullspace has dimension %d", left_ns.shape[1]
+            if len(left_ns.shape) > 1 else 0)
     inc_minimal = set()
     (problem, indicators) = con_helpers.create_milp_problem(
         left_ns, metabolites, Model, Variable, Constraint, Objective)