0e435a0b88ad8cb3749b6a29e738f63f458862f3
Moritz E. Beber <midnighter@posteo.net>
2023-10-29 23:35:57 +0100

    chore: update versioneer

--- src/memote/__init__.py
+++ src/memote/__init__.py
@@ -37,3 +37,6 @@ from memote.suite.api import *
 from memote.suite.results import *
 from memote.suite.reporting import *
 from memote.jinja2_extension import *
+
+from . import _version
+__version__ = _version.get_versions()['version']
